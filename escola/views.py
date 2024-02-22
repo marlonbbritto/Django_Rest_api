a683e443e8d31d111555f4cce36c492cd9c0766c
marlonbbritto <marlonbbritto>
2024-02-21 21:50:46 -0300

    criando a viewset e configurando as urls

--- escola/views.py
+++ escola/views.py
@@ -1,9 +1,14 @@
-from django.http import JsonResponse
-from escola.models import Aluno
+from rest_framework import viewsets
+from escola.models import Aluno,Curso
+from escola.serializer import AlunoSerializer,CursoSerializer
 
-def alunos(request):
-    if request.method == "GET":
-        response = Aluno
-        return JsonResponse(aluno)
+class AlunosViewSet(viewsets.ModelViewSet):
+    '''Exibindo todos os alunos e alunas'''
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
 
+class CursosViewSet(viewsets.ModelViewSet):
+    '''Exibindo todos os cursos'''
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
 
